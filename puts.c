430119156d8d856b2558062ded0a22c10baa6930
Kyshl <keshlill94@gmail.com>
2023-05-18 12:29:17 -0700

    puts

---
+++ puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print the string in the given function
+ *
+ * Description: using the main function
+ * prints "how about dancing to the solved code errors 
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"how about dancing to the solved code errors");
+	return (0);
+}
