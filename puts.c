aeab4a0e71519fe76e7289cbbbe632e4ce146499
1234beth <elizachela98@gmail.com>
2023-05-20 06:56:26 -0700

    header

--- puts.c
+++ puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - print the string in the given function
- *
- * Description: using the main function
- * prints "how about dancing to the solved code errors 
- * Return: 0
- */
+* main - print the string in the given function
+*
+* Description: using the main function
+* prints: "how about dancing to the solved code errors"
+* Return: 0
+*/
 int main(void)
 {
 	puts("\"how about dancing to the solved code errors");
